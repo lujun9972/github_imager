1afc87e35a85bd392653f44c4a2bc1852ea2b488
darksun <lujun9972@gmail.com>
2019-06-16 02:46:47 +0800

    update

--- github-convert.el
+++ github-convert.el
@@ -1,6 +1,7 @@
 (require 'vc-git)
 (defun github-convert-link (&optional filename)
-  (let* ((filename (or filename (buffer-file-name)))
+  (interactive)
+  (let* ((filename (or filename (read-file-name "Select a file")))
          (root-path (vc-git-root filename))
          ;; (asset-abs-path (expand-file-name default-directory filename))
          (relative-path (file-relative-name filename root-path))
@@ -9,11 +10,19 @@
             (insert (vc-git-dir-extra-headers root-path))
             (when (re-search-backward "^Remote[[:blank:]]+:[[:blank:]]+\\([^[:blank:]\r\n]+\\)" nil t)
               (match-string 1))))
+         (branch
+          (with-temp-buffer
+            (insert (vc-git-dir-extra-headers root-path))
+            (when (re-search-backward "^Branch[[:blank:]]+:[[:blank:]]+\\([^[:blank:]\r\n]+\\)" nil t)
+              (match-string 1))))
          (converted-path (when (string-match "^git@github.com:\\([^/]+\\)/\\(.+\\).git" remote-url)
                            (let ((site "raw.githubusercontent.com")
                                  (user (match-string-no-properties 1 remote-url))
                                  (repo (match-string-no-properties 2 remote-url)))
                              (format "https://%s/%s/%s/source/%s" site user repo relative-path)))))
+    (when (interactive-p)
+      (message "%s" converted-path)
+      (kill-new converted-path))
     converted-path))
 
 (defcustom github-convert-rules
@@ -39,7 +48,7 @@ NUM specifies which parenthesized expression in the regexp should be replaced.")
                      (asset-abs-path (expand-file-name asset-path (file-name-directory filename))))
                 (unless (url-type (url-generic-parse-url asset-path)) ;; 判断是否为绝对路径的URI
                   (if (not (file-exists-p asset-abs-path))
-                      (message "ORG2ISSUE: [WARN] File %s in hyper link does not exist, org file: %s." asset-abs-path filename)
+                      (message "[WARN] File %s in hyper link does not exist, org file: %s." asset-abs-path filename)
                     (let ((converted-path (github-convert-link asset-abs-path)))
                       (when converted-path
                         (setf (buffer-substring asset-path-begin asset-path-end) converted-path))))))))))
